e585e2bb68be7fbca9dad5ec85dfa560518dce4a
guipeng <aceguipeng@gmail.com>
2024-10-09 23:40:32 +0800

    修复populate在v5不生效的问题

--- src/app/[lang]/utils/get-page-by-slug.ts
+++ src/app/[lang]/utils/get-page-by-slug.ts
@@ -1,55 +1,5 @@
+"use server"
 import {fetchAPI} from "@/app/[lang]/utils/fetch-api";
-
-const populate = {
-  contentSections: {
-    on: {
-      "sections.hero": {
-        populate: "*",
-      },
-      "sections.features": {
-        populate: "*",
-      },
-      "sections.bottom-actions": {
-        populate: "*",
-      },
-      "sections.feature-columns-group": {
-        populate: "*",
-      },
-      "sections.feature-rows-group": {
-        populate: "*",
-      },
-      "sections.testimonials-group": {
-        populate: {
-          testimonials: {
-            populate: "*",
-          },
-        },
-      },
-      "sections.large-video": {
-        populate: "*",
-      },
-      "sections.rich-text": {
-        populate: "*",
-      },
-      "sections.pricing": {
-        populate: {
-          plans: {
-            populate: "*",
-          },
-        },
-      },
-      "sections.lead-form": {
-        populate: "*",
-      },
-      "sections.heading": {
-        populate: "*",
-      },
-    },
-  },
-  seo: {
-    populate: "*",
-  },
-}
 
 export async function getPageBySlug(slug: string, lang: string) {
   const token = process.env.STRAPI_API_TOKEN;
@@ -59,8 +9,7 @@ export async function getPageBySlug(slug: string, lang: string) {
     filters: {
       slug
     },
-    locale: lang,
-    populate: populate
+    locale: lang
   };
   const options = {headers: {Authorization: `Bearer ${token}`}};
   return await fetchAPI(path, urlParamsObject, options);
